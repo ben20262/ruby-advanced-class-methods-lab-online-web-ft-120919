df7d33dd71310d2ffec7947f2fe5ec6f03d405d5
Benjamin Small <ben20262@aim.com>
2019-12-18 20:21:52 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -52,8 +52,8 @@ class Song
   def self.new_from_filename(file_name)
     inst = self.new
     song_array = file_name.chomp(".mp3").split(" - ")
-    isnt.artist_name = song_array[0]
-    isnt.name = song_array[1]
+    inst.artist_name = song_array[0]
+    inst.name = song_array[1]
     inst
   end
 
