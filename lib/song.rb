b5553974b1fa9c6a2412f7cde377afe72240a497
Benjamin Small <ben20262@aim.com>
2019-12-18 20:06:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -38,7 +38,11 @@ class Song
   end
 
   def self.find_or_create_by_name(name)
-
+    if self.find_by_name(name)
+      return self.find_by_name(name)
+    else
+      self.create_by_name(name)
+    end
   end
 
   def self.alphabetical
