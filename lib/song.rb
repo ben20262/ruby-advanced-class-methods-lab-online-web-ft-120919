06c384981ca6ba4fb3882106c8a3779516b194c5
Benjamin Small <ben20262@aim.com>
2019-12-18 19:51:29 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -18,27 +18,27 @@ class Song
   end
 
   def self.new_by_name(name)
-    
+
   end
 
   def self.create_by_name(name)
-    
+
   end
 
   def self.find_by_name(name)
-    
+
   end
 
   def self.find_or_create_by_name(name)
-    
+
   end
 
   def self.alphabetical
-    
+
   end
 
   def self.new_from_filename(name)
-    
+
   end
 
   def self.destroy_all
