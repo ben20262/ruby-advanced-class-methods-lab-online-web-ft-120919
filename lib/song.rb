9532eceb7a35233d11675ecda3312b603c624af7
Benjamin Small <ben20262@aim.com>
2019-12-18 18:51:25 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,50 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    self.class.all << self
+    puts @@all
+    self
+  end
+
+  def self.new_by_name(name)
+    @name = name
+  end
+
+  def self.create_by_name(name)
+    @name = name
+    @@all << self
+    self
+  end
+
+  def self.find_by_name(name)
+    @@all.find{|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    if !!self.find_by_name(name)
+      return self.find_by_name(name)
+    else
+      self.create_by_name(name)
+    end
+  end
+
+  def self.alphabetical
+    self.name.sort
+  end
+
+  def self.new_from_filename(file_name)
+    song_array = file_name.chomp(".mp3").split(" - ")
+    @name = song_array[0]
+    @artist_name = song_array[1]
+  end
+
+  def self.create_from_filename(file_name)
+    self.new_from_filename(file_name)
+    self.create
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
 end
